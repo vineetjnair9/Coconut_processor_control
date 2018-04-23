9bef95c6535b8db8dc71efefa1422d70b0f95fde
Junaid <junaidmaknojia786@berkeley.edu>
2018-04-23 14:07:26 -0700

    Added code

---
+++ StepperTest/StepperTest.ino
@@ -0,0 +1,40 @@
+/* 
+This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
+It won't work with v1.x motor shields! Only for the v2's with built in PWM
+control
+
+For use with the Adafruit Motor Shield v2 
+---->	http://www.adafruit.com/products/1438
+*/
+
+
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+// Or, create it with a different I2C address (say for stacking)
+// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
+
+// Connect a stepper motor with 200 steps per revolution (1.8 degree)
+// to motor port #2 (M3 and M4)
+Adafruit_StepperMotor *myMotor = AFMS.getStepper(100, 2);
+Adafruit_StepperMotor *myMotor1 = AFMS.getStepper(200, 1);
+
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Stepper test!");
+
+  AFMS.begin();  // create with the default frequency 1.6KHz
+  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+  
+  myMotor->setSpeed(255); 
+  //myMotor1->setSpeed(50);// 50 rpm   
+}
+
+void loop() {
+  myMotor->step(10, BACKWARD, SINGLE);  
+ // myMotor1->step(100, FORWARD, SINGLE); 
+}
