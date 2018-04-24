2685802a0bd2310ad3948d3e1c06e17c7c2b81db
VINEET NAIR <jvineet9@BERKELEY.EDU>
2018-04-24 15:15:25 -0700

    modified code

--- Combined/Combined.ino
+++ Combined/Combined.ino
@@ -5,12 +5,21 @@
 //#include <utility/Adafruit_MS_PWMServoDriver> 
 
 Servo myServo;   // Define any servo name
-
-// 2 Continuous rotation servos
-// 2 Steppers
-// Motor controller for actuator
-// Motor shields
-// 1 Linear actuator
+int buttonMain = 13; // ON/OFF Button overall
+int buttonSaw = 12; // Button to turn mechanism ON/OFF after saw 
+int actuatorPin = 11; // Pin on shield connected to motor controller
+int tServoPin = 10; 
+int cServoPin = 9;
+int drillSteps = 100; // both of these should be the same for the same motor type right?
+int meatSteps = 200; // Steps per revolution
+int drillPort = 2; // Ports M3 and M4
+int meatPort = 1; // Ports M1 and M2
+int drillSpeed = 255; // default speed (rpm)
+int meatSpeed = 255; 
+int PWMopen = 100; // To open the clamps with actuator
+int PWMclose = 20; // To close clamps
+int drillRev = 1600; // no. of revolutions needed to drill into coconut
+int meatRev = 1600; // no. of revolutions needed to scrape off all meat
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); // Create the motor shield object with the default I2C address
 // Or, create it with a different I2C address (say for stacking)
@@ -18,61 +27,70 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield(); // Create the motor shield o
 
 // Connect a stepper motor with 200 steps per revolution (1.8 degree)
 // to motor port #2 (M3 and M4)
-Adafruit_StepperMotor *myMotor = AFMS.getStepper(100, 2);
-Adafruit_StepperMotor *myMotor1 = AFMS.getStepper(200, 1);
+Adafruit_StepperMotor *drillMotor = AFMS.getStepper(100, 2);
+Adafruit_StepperMotor *meatMotor = AFMS.getStepper(200, 1);
+Servo cServo;   // clamp servo
+Servo tServo;   // track servo
 
 void setup() {
+  drillMotor->setSpeed(drillSpeed); // drill motor 
+  meatMotor->setSpeed(meatSpeed); // meat scraper default speed
+}
+
+ void loop() {
   
- Adafruit_MotorShield AFMS = Adafruit_MotorShield(); // Create the motor shield object with the default I2C address
- Servo cServo;   // clamp servo
- Servo tServo;   // track servo
-  
-// drilling hole code
-  myMotor->setSpeed(255); // drill motor   
-  myMotor->step(1600, BACKWARD, SINGLE);  
-  tServo.attach(10);
-  tServo.write(180); //clockwise rotation to push coconut into 
-  delay(2000); //rotation duration in ms (how long to reach depth of coconut cutting)
-  tServo.detach(); //detach servo to prevent “creeping” effect
+  // drilling hole code
+  // step(#steps, direction, steptype)  
+  // steptype = double for higher torque
+  drillMotor->step(drillRev, BACKWARD, DOUBLE);  
+  tServo.attach(tServoPin);
+
+  // May have to adjust these angles to avoid interference w/ clamps while cutting/drilling
+  tServo.write(180); // clockwise rotation to push coconut into 
+  delay(2000); // rotation duration in ms (how long to reach depth of coconut cutting)
+  tServo.detach(); // detach servo to prevent “creeping” effect
   delay(1000); // let the hole become smoooth
-  tServo.attach(10);
+  tServo.attach(tServoPin);
   tServo.write(0); //counterclockwise rotation to pull coconut away
-  delay(2000); //rotation duration in ms
-  tServo.detach(); //detach servo to prevent “creeping” effect
-  myMotor->setSpeed(0); // stop drill motor   
-  myMotor->step(1600, BACKWARD, SINGLE);  
+  delay(2000); // rotation duration in ms
+  tServo.detach(); // detach servo to prevent “creeping” effect
+
+  // This might not be needed, I think motor stops automatically
+  // drillMotor.setSpeed(0); // stop drill motor   
+  drillMotor->step(drillRev, BACKWARD, SINGLE);
+  // maybe use MICROSTEP for smoother motion?
 
 // water draining
-  cServo.attach(10);
-  cServo.write(180); //clockwise rotation
-  delay(2000); //rotation duration in ms (stop at 180 degrees to drain water)
-  cServo.detach(); //detach servo to prevent “creeping” effect 
+  cServo.attach(cServoPin);
+  cServo.write(180); // clockwise rotation
+  delay(2000); // rotation duration in ms (stop at 180 degrees to drain water)
+  cServo.detach(); // detach servo to prevent “creeping” effect 
   delay(2000); // time it'll take to drain water
-  
   delay(5000); // wait for saw to turn on (or include switch case for a switch or such)
 
 // saw cutting  
-  tServo.attach(10); 
-  tServo.write(0); //forward direction into saw 
-  delay(2000); //move until in the middle of saw (in ms)
-  tServo.detach(); //detach servo to prevent “creeping” effect
-  cServo.attach(10);
-  cServo.write(0); //counterclockwise rotation, switch if needed the other way
+  tServo.attach(tServoPin); 
+  tServo.write(0); // forward direction into saw 
+  delay(2000); // move until in the middle of saw (in ms)
+  tServo.detach(); // detach servo to prevent “creeping” effect
+  cServo.attach(cServoPin);
+  cServo.write(0); // counterclockwise rotation, switch if needed the other way
   delay(2000); //rotation speed as it cuts (in ms)
-  cServo.detach(); //detach servo to prevent “creeping” effect
+  cServo.detach(); // detach servo to prevent “creeping” effect
 
 // start linear actuator and open coconut
-  analogWrite(pin, value); //tells motor driver to send extra voltage to open linear actuator
+  analogWrite(actuatorPin, PWMopen); //tells motor driver to send extra voltage to open linear actuator
 
 //meat scraper
-  tServo.attach(10); 
+  tServo.attach(tServoPin); 
   tServo.write(0); //forward direction into meat scraper 
   delay(2000); //move until lined up with meat scraper (in ms)
   tServo.detach(); //detach servo to prevent “creeping” effect
-  analogWrite(pin, value); //close coconut
+  analogWrite(actuatorPin, PWMclose); // close coconut (linear actuator) 
 
-  myMotor2->setSpeed(255); // meat scraper motor start
-  myMotor2->step(1600, BACKWARD, SINGLE);  
-  myMotor2->setSpeed(0); // stop motor
-  analogWrite(pin, value); //open coconut
+  meatMotor->step(meatRev, BACKWARD, SINGLE);  
+  //meatMotor->setSpeed(0); // stop motor
+  analogWrite(actuatorPin, PWMopen); //open coconut
  }
+ 
+
